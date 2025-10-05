6687416190ba3bf6d69b2d2d019cb860a0ff8bac
Fzurbonsen <frederic.zur-bonsen@bluewin.ch>
2025-10-06 00:01:19 +0200

    [lab1]: updated benchmarks

--- lab1/inputs/benchmark/random_access.s
+++ lab1/inputs/benchmark/random_access.s
@@ -1,29 +1,30 @@
 # random_access.s
 .data
-array: .space 32768          # 32 KB array
-index: .word 0, 32, 128, 256, 512, 1024, 2048, 4096, 8192, 0, 128, 256, 512, 1024, 2048, 4096
+array: .space 32768
+index: .word 0,32,128,256,512,1024,2048,4096,8192,0,128,256,512,1024,2048,4096
 
 .text
 .globl main
 main:
-    la   $t0, array
-    la   $t1, index
-    li   $t2, 16            # number of indices
-    li   $t7, 8192          # max valid index (32768 bytes / 4 bytes per word)
+    la $t0, array
+    la $t1, index
+    li $t2, 16
+    li $t7, 8192
 
 loop_rand:
-    lw   $t3, 0($t1)        # load index value
-    bge  $t3, $t7, skip_access   # skip invalid indices
-
-    sll  $t4, $t3, 2        # multiply by 4
-    add  $t5, $t0, $t4      # compute address
-    lw   $t6, 0($t5)        # load array[index]
+    lw $t3, 0($t1)
+    bge $t3, $t7, skip_access
+    sll $t4, $t3, 2
+    add $t5, $t0, $t4
+    lw $t6, 0($t5)
     addi $t6, $t6, 1
-    sw   $t6, 0($t5)
+    sw $t6, 0($t5)
 
 skip_access:
     addi $t1, $t1, 4
     addi $t2, $t2, -1
     bgtz $t2, loop_rand
 
-    syscall
+# exit program
+li $v0, 10
+syscall
